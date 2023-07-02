739848187a82b396d9ae2ad726c8b0215ea63e0e
Riya Patel <93567526+riyapat@users.noreply.github.com>
2023-07-02 17:11:29 +0530

    Create Matric rotation problem.cpp

---
+++ Matric rotation problem.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void rotate(vector<int>& matrix){
+    for(int i=0;i<matrix.size();i++){
+        for(int j=i;j<matrix[0].size();j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+    for(int i=0;i<matrix.size();i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+}
+
+
+int main(){
+    
+    rotate(matrix);
+
+}
